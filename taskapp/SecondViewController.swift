5aad3c6eb033d9a278aa64d8e0957e831c0d25ca
Daichiーmorishita <wankoro.showman100@gmail.com>
2020-08-23 15:49:03 +0900

    Initial Commit

---
+++ taskapp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  taskapp
+//
+//  Created by 森下大地 on 2020/08/23.
+//  Copyright © 2020 daichi.morishita. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
